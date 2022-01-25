00dd2963079862f79917a4e3e57b4feda255478b
jared <jared.torp@eagles.oc.edu>
2022-01-24 21:21:48 -0600

    added location

---
+++ app/models/location.model.js
@@ -0,0 +1,30 @@
+//const { DataTypes } = require("sequelize/types");
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize, Sequelize) => {
+    const Location = sequelize.define("location", {
+
+      locationID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, 
+        unique: true,
+        allowNull: false
+      },
+      building: {
+        type: DataTypes.STRING
+      },
+      roomNum: {
+          type: DataTypes.STRING
+      },
+      desc:{
+        type: DataTypes.STRING
+      },
+
+    },
+      {
+        tableName: 'locations'
+
+    });
+
+    return Location;
+  };
