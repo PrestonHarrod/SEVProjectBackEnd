f5210ebcc7c2be588b50bc3242ca623a3f676248
jared <jared.torp@eagles.oc.edu>
2022-01-24 21:05:20 -0600

    Requests done

---
+++ app/models/request.model.js
@@ -0,0 +1,32 @@
+//const { DataTypes } = require("sequelize/types");
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize, Sequelize) => {
+    const Request = sequelize.define("request", {
+
+      requestID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true, 
+        unique: true,
+        allowNull: false
+      },
+      userID: {
+        type: DataTypes.INTEGER
+      },
+      Type: {
+          type: DataTypes.STRING
+      },
+      desc: {
+          type:DataTypes.STRING
+      },
+
+
+
+    },
+      {
+        tableName: 'requests'
+
+    });
+
+    return Request;
+  };
