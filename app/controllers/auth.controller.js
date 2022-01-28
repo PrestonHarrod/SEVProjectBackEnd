8e0341f15d1d930622b6a2ecaf828d86e9ae35d9
PrestonHarrod <prestonharrod@gmail.com>
2022-01-28 14:29:39 -0600

    logs in user and creates logintoken

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -31,10 +31,7 @@ exports.login = async (req, res) => {
 
   let user = {};
   //let eMail = null;
-  let advisorID = null;
-  let studentID = null;
   let userID = null;
-  let fName = null;
   let token = null;
 
   // Look for an advior in the database
@@ -42,14 +39,11 @@ exports.login = async (req, res) => {
   await User.findOne({ where : {email:email}})
     .then(data => {
         if (data != null) {
-        let advisor= data.dataValues;
-        token = jwt.sign({ id: advisor.email }, authconfig.secret, {expiresIn: 86400}); // 24 hours
-        email = advisor.email;
-        advisorID = advisor.advisorID;
-        studentID = null;
-        adminID = null;
-        userID = advisor.advisorID;
-        fName = advisor.fName;
+        let founduser = data.dataValues;
+        token = jwt.sign({ id: founduser.email }, authconfig.secret, {expiresIn: 86400}); // 24 hours
+        email = founduser.email;
+        userID = founduser.userID
+        fName = founduser.fName;
         userFound = true;
 
         }
@@ -57,53 +51,6 @@ exports.login = async (req, res) => {
         res.status(401).send({
           message: err.message || "Error looking up User"
         });
-        return;
-    });
-
-    //Look for admin in DB
-  await Admin.findOne({ where : {email:email}})
-    .then(data => {
-        if (data != null) {
-        let admin= data.dataValues;
-        token = jwt.sign({ id: admin.email }, authconfig.secret, {expiresIn: 86400}); // 24 hours
-        email = admin.email;
-        adminID = admin.adminID;
-        studentID = null;
-        advisorID = null
-        userID = admin.adminID;
-        fName = admin.fName;
-        userFound = true;
-
-        }
-    }).catch(err => {
-        res.status(401).send({
-          message: err.message || "Error looking up User"
-        });
-        return;
-    });
-
-    // Look for a student in the database
-    //comment
-      await Student.findOne({where : {email:email}
-      })
-      .then(data => {
-        if (data != null) {         
-            let student = data.dataValues;
-           // console.log("Data Values: " + data.dataValues.studentID)
-            token = jwt.sign({ id: student.email }, authconfig.secret, {expiresIn: 86400}); // 24 hours
-            email = student.email;
-            advisorID = null
-            adminID = null;
-            studentID = student.studentID;
-            userID  = student.studentID;
-            fName = student.fName;
-            userFound = true;
-         }
-
-      }).catch(err => {
-        res.status(401).send({
-          message: err.message || "Count not find user"
-       });
         return;
     });
     if (!userFound) {
@@ -114,23 +61,18 @@ exports.login = async (req, res) => {
     }
   let tokenExpireDate =new Date();
   tokenExpireDate.setDate(tokenExpireDate.getDate() + 1);
-  const session = {
+  const login = {
     token: token,
-    advisorID : advisorID,
-    studentID : studentID,
-    adminID : adminID,
+    userID : userID,
     expireDate: tokenExpireDate
   };
-  console.log(session.studentId)
-  Session.create(session)
+  console.log(login.userID)
+  LoginToken.create(login)
     .then(data => {
       let userInfo = {
         user : fName,
-        studentID : studentID,
-        advisorID: advisorID,
-        adminID : adminID,
-        userId : userID,
-        token : session.token
+        userID : userID,
+        token : login.token
       };
       
       res.send(userInfo);
