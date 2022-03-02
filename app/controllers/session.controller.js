893c054835ee91c26687b84fc49165aa23c21fd5
Logan Christopher <lchris20@gmail.com>
2022-03-02 16:19:52 -0600

    had to change the request validation requirement

--- app/controllers/session.controller.js
+++ app/controllers/session.controller.js
@@ -5,7 +5,7 @@ const Op = db.Sequelize.Op;
 // Create and Save a new Session
 exports.create = (req, res) => {
     // Validate request
-    if (!req.body.sessionID) {
+    if (!req.body.studentID) {
       res.status(400).send({
         message: "Content can not be empty!"
       });
